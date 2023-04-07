51e4c22ce20bc538ddde287ef91d1e2d7d759d7c
Woo-bi <39858246+Choo-bi@users.noreply.github.com>
2023-04-07 16:02:24 +0900

    Origining...

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const store = createStore(rootReducer);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
