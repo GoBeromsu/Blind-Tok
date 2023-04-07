1168a3638d3408bd009535b0c55fc7606dbbfb84
Woo-bi <39858246+Choo-bi@users.noreply.github.com>
2023-04-07 16:35:55 +0900

    directory organizing..

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import rootReducer from './reducers';
+import rootReducer from './store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
