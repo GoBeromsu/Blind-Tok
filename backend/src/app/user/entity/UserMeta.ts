e0ed99994684eae87c6560f527c8b1d218057e72
goberomsu <gobeumsu@gmail.com>
2023-04-08 23:13:32 +0900

    Feat : User && UserMeta 테이블 작성
    API 명세가 불분명해서 작업을 지속하기 어려움
    내일 API 명세 끝낼 것

---
+++ backend/src/app/user/entity/UserMeta.ts
@@ -0,0 +1,19 @@
+import {COLUMN_TYPE_BIGINT, COLUMN_TYPE_TEXT} from "@common/CommonConstants";
+import {BaseEntity, Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn} from "typeorm";
+import User from "./User";
+
+@Entity()
+export default class UserMeta extends BaseEntity {
+  @PrimaryGeneratedColumn({type: COLUMN_TYPE_BIGINT})
+  userid: number;
+  
+  @Column({type: COLUMN_TYPE_TEXT})
+  profilemesage: string;
+
+  @Column({type: COLUMN_TYPE_TEXT})
+  profilepictureurl: string;
+  
+  @OneToOne(() => User, user => user.userid)
+  @JoinColumn({name: "userid"})
+  user: User;
+}
