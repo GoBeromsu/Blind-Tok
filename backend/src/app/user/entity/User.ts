70b38736df2d22447ac8c6101aef6b671266c39d
goberomsu <gobeumsu@gmail.com>
2023-05-03 00:46:02 +0900

    Fix : git ignore
    우리 프로젝트 환경에 알맞게 .gitignore을 추가했습니다.
    - window, macos, react, node를 대상으로 합니다

--- backend/src/app/user/entity/User.ts
+++ backend/src/app/user/entity/User.ts
@@ -16,12 +16,12 @@ import {COLUMN_TYPE_BIGINT, COLUMN_TYPE_TEXT} from "@common/CommonConstants";
 export default class User extends BaseEntity {
   @PrimaryGeneratedColumn({type: COLUMN_TYPE_BIGINT})
   userid: number;
-
+  //
   @Column({type: COLUMN_TYPE_TEXT})
-
+  name: string;
+  // 친구가 아닌 사람들이 보면 보일 별명
+  @Column({type: COLUMN_TYPE_TEXT})
   nickname: string;
-  @Column({type: COLUMN_TYPE_TEXT})
-  fakename: string;
 
   @Column({type: COLUMN_TYPE_TEXT})
   email: string;
