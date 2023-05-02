53064034eda80f445402c4506fbad1bb8b01b112
goberomsu <gobeumsu@gmail.com>
2023-05-03 01:53:33 +0900

    Feat : 단일 유저, 복수 유저, 유저 등록 서비스 추가

--- backend/src/app/user/service/UserService.ts
+++ backend/src/app/user/service/UserService.ts
@@ -1,9 +1,20 @@
 import {txProcess} from "./../../../lib/db";
 import User from "@user/entity/User";
+import UserRelation from "@user/entity/UserRelation";
 
 export async function getUserInfo(userid: number) {
   return await User.findOne({where: {userid}, relations: {meta: true}});
 }
-
-//TODO: 유저를 추가하는 서비스, 유저를 처음 등록 할 때, email이랑 fake name 주지 않나
-export async function addUser({fackname}: {fackname: string; email: string}) {}
+export async function getUsersInfo() {
+  return await User.find({relations: {meta: true}});
+}
+export async function addUser({name, email}: {name: string; email: string}) {
+  // TODO: 현재 닉네임을 부여하는 기능이 없어서 이름과 동일하게 nickname을 부여하겠음
+  const nickname = name;
+  return await txProcess(async manager => {
+    const repository = manager.getRepository(User);
+    const friendRepository = manager.getRepository(UserRelation);
+    const user = await repository.save({name: name, nickname: nickname, email: email});
+    return await repository.save(user);
+  });
+}
