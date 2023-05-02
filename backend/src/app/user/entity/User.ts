78ad3580c62f331abfe88ed8810dbcf2cbf28d45
goberomsu <gobeumsu@gmail.com>
2023-05-03 01:50:51 +0900

    Fix : User 엔티티의 모호한 필드 이름 수정
    - nickname과 fakename은 둘이 의미가 조금 겹침
    - name과 nickname으로 구분

--- backend/src/app/user/entity/User.ts
+++ backend/src/app/user/entity/User.ts
@@ -1,13 +1,4 @@
-import {
-  BaseEntity,
-  Column,
-  CreateDateColumn,
-  Entity,
-  OneToMany,
-  OneToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from "typeorm";
+import {BaseEntity, Column, CreateDateColumn, Entity, OneToMany, OneToOne, PrimaryGeneratedColumn, UpdateDateColumn} from "typeorm";
 import UserMeta from "./UserMeta";
 import UserRelation from "./UserRelation";
 import {COLUMN_TYPE_BIGINT, COLUMN_TYPE_TEXT} from "@common/CommonConstants";
@@ -16,7 +7,7 @@ import {COLUMN_TYPE_BIGINT, COLUMN_TYPE_TEXT} from "@common/CommonConstants";
 export default class User extends BaseEntity {
   @PrimaryGeneratedColumn({type: COLUMN_TYPE_BIGINT})
   userid: number;
-  //
+  // 실제 사용자의 이름
   @Column({type: COLUMN_TYPE_TEXT})
   name: string;
   // 친구가 아닌 사람들이 보면 보일 별명
