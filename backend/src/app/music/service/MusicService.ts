28185cdd4d110408f4bd1d951a80fafa1b03139b
goberomsu <gobeumsu@gmail.com>
2023-05-04 05:41:04 +0900

    Feat : Music 서비스 작성

---
+++ backend/src/app/music/service/MusicService.ts
@@ -0,0 +1,64 @@
+import {txProcess} from "@lib/db";
+import Music from "../entity/Music";
+import User from "@user/entity/User";
+
+export async function getMusicInfo(musicid: number) {
+  return await Music.findOne({where: {musicid}, relations: {user: true}});
+}
+export async function getMusicsInfo() {
+  return await Music.find({relations: {user: true}});
+}
+export async function addMusic({
+  userid,
+  fileName,
+  filePath,
+  mimeType,
+  duration,
+  fileSize,
+}: {
+  userid: number;
+  fileName: string;
+  filePath: string;
+  mimeType: string;
+  duration: number;
+  fileSize: number;
+}) {
+  const nickname = name;
+  return await txProcess(async manager => {
+    const repository = manager.getRepository(Music);
+    const result = repository.save({userid, fileName, filePath, mimeType, duration, fileSize});
+    return result;
+  });
+}
+export async function editUser(
+  musicid: number,
+  {
+    userid,
+    fileName,
+    filePath,
+    mimeType,
+    duration,
+    fileSize,
+  }: {
+    userid: number;
+    fileName: string;
+    filePath: string;
+    mimeType: string;
+    duration: number;
+    fileSize: number;
+  },
+) {
+  return await txProcess(async manager => {
+    const repository = manager.getRepository(Music);
+    return repository.update(musicid, {
+      userid,
+      fileName,
+      filePath,
+      mimeType,
+      duration,
+      fileSize,
+    });
+  });
+}
+//TODO: 추후에 필요하면 추가로 생성합니다
+export async function removeMusic() {}
