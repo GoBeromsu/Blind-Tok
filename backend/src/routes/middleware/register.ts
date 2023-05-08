ff012f73eb47fc0b87bf8d8f56970fc98b4f0a5d
goberomsu <gobeumsu@gmail.com>
2023-05-08 13:46:12 +0900

    Feat : 미들웨어에 register 등록

--- backend/src/routes/middleware/register.ts
+++ backend/src/routes/middleware/register.ts
@@ -4,14 +4,16 @@ import {fastifyView} from "@fastify/view";
 import formbody from "@fastify/formbody";
 import ejs from "ejs";
 import fastifyCookie from "@fastify/cookie";
-
+import oauth2 from "@fastify/oauth2";
+import {googleAuth} from "@config/auth.config";
+import {adam} from "@config/adam.config";
 export default async function (fastify: FastifyInstance) {
   // FormBody
   fastify.register(formbody);
 
   //Cookie
   fastify.register(fastifyCookie, {
-    // secret : 나중에 구글 sso 시그니쳐 추가해야함
+    secret: adam?.token.secret, //나중에 구글 sso 시그니쳐 추가해야함
     parseOptions: {httpOnly: true},
   });
   //파일 스토리지를 다루기 위함
@@ -23,4 +25,6 @@ export default async function (fastify: FastifyInstance) {
   });
 
   fastify.register(fastifyView, {engine: {ejs}});
+  console.log(googleAuth.callbackUri);
+  fastify.register(oauth2, googleAuth);
 }
