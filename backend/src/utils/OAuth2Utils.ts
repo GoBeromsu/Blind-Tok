f6b7a51914c4b071d01acb91158a2c17c2f22015
goberomsu <gobeumsu@gmail.com>
2023-05-08 12:26:01 +0900

    Feat : JWT parser && SSO checker

--- backend/src/utils/OAuth2Utils.ts
+++ backend/src/utils/OAuth2Utils.ts
@@ -1,7 +1,37 @@
 import jwt from "jsonwebtoken";
 import {adam} from "@config/adam.config";
+import {LOGIN_STATUS} from "@user/UserConstants";
+import {getUserLogin} from "@user/service/UserLoginService";
+
 const {token} = adam;
 
+export function parseJWT(token: string) {
+  try {
+    let base64Url = token.split(".")[1];
+    let base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+    let jsonPayload = decodeURIComponent(
+      atob(base64)
+        .split("")
+        .map(function (c) {
+          return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+        })
+        .join(""),
+    );
+    return JSON.parse(jsonPayload);
+  } catch (err) {
+    return JSON.parse("{}");
+  }
+}
+export const checkSSO = async (type: string, ssoid: string, params: {email?: string; nickname?: string}) => {
+  const oauth = await getUserLogin(ssoid);
+  if (oauth) {
+    const user = oauth.user;
+    //FIXME: 로그인 하는데 파라미터를 더 받기 위해 가져옵니다 - 범수
+    return {status: LOGIN_STATUS.LOGIN, auth: {type, ssoid, ...params}, user}; // 있으면 유저를 가저 온다.
+  } else {
+    return {status: LOGIN_STATUS.REGISTER, auth: {type, ssoid, ...params}};
+  }
+};
 // JWT를 생성하고 서명하는 함수를 정의합니다.
 // 'payload'는 토큰에 포함할 데이터이고, 'expiresIn'은 토큰의 만료 시간입니다.
 export const signJWT = async (payload: any, expiresIn = token.expiresIn) => {
