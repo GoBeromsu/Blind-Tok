3c034a5a4eab90516d9fe60604b5cd1721646722
goberomsu <gobeumsu@gmail.com>
2023-05-10 22:49:11 +0900

    Feat : React-query 설정

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
-import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+import {QueryClient, QueryClientProvider} from "react-query";
 const queryClient = new QueryClient();
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
