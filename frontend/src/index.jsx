b3e839c1235700a9a87edf43711427a9aa2eddce
goberomsu <gobeumsu@gmail.com>
2023-05-10 23:37:29 +0900

    Feat : Google Auth 클라이언트 추가

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import {QueryClient, QueryClientProvider} from "react-query";
+import {GoogleOAuthProvider} from "@react-oauth/google";
+
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
 );
