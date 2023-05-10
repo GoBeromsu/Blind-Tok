f18fb928460bef56786352470afdb512e7533f70
goberomsu <gobeumsu@gmail.com>
2023-05-11 02:26:11 +0900

    Feat : LifeCycle 추가
    jwt 토큰을 로그인하면 자동으로 body에 저장해서 움직이도록 전처리를 함으로써 유저 로그인 인증을 합니다

--- backend/src/routes/middleware/index.ts
+++ backend/src/routes/middleware/index.ts
@@ -2,6 +2,8 @@ import {FastifyInstance} from "fastify";
 import {fastifyView} from "@fastify/view";
 import ejs from "ejs";
 import register from "./register";
+import lifecycle from "./lifecycle";
 export default async function (fastify: FastifyInstance) {
   register(fastify);
+  lifecycle(fastify);
 }
