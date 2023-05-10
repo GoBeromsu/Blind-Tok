4e6d56881ca6bf8c7592c4b6c6f0e0e11f6b4118
goberomsu <gobeumsu@gmail.com>
2023-05-11 03:21:44 +0900

    Fix : 메인은 조곰씩 수정하면 되리 듯

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,11 +7,18 @@ import "./components/style/App.css";
 // import ChatList from "./components/views/Chat/ChatList";
 // import ChatRoom from "./components/views/Chat/ChatRoom";
 
-import {BrowserRouter as Router, Route, Routes, Navigate} from "react-router-dom";
+import {BrowserRouter as Router, Route, Routes, Navigate, Outlet} from "react-router-dom";
 import {getAuthUserQuery, getUserInfoQuery} from "@data/user/query";
 import Loading from "react-loading";
-import Auth from "./hoc/Auth";
+
 import Login from "@views/Login/Login";
+import Auth from "@hoc/Auth";
+import {useRecoilState} from "recoil";
+import {userState} from "@data/user/state";
+import MainComponent from "@views/MainPage/MainComponent";
+import FriendList from "@views/Friend/FriendList";
+import ChatList from "@views/Chat/ChatList";
+import UserProfile from "@views/User/UserProfile";
 
 export default function App() {
   return (
@@ -24,54 +31,66 @@ export default function App() {
 
 function AppRoutes() {
   const {isLoading, isError, data, error} = getAuthUserQuery();
-
-  const user_id = 1;
-  // const [user, setUser] = useState(getUserInfoQuery(user_id));
+  const [loginUser, setLoginUser]: any = useRecoilState(userState);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   const [sidebarWidth, setSidebarWidth] = useState(windowWidth <= 768 ? 50 : 200);
   const [contentWidth, setContentWidth] = useState(windowWidth - sidebarWidth);
 
   useEffect(() => {
-    // setUser(getUserInfoQuery(user_id));
-    const handleResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
+    if (loginUser == null && data != null) {
+      setLoginUser(data);
+    }
+  }, [data]);
 
-    window.addEventListener("resize", handleResize);
-
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
-  }, []);
-
-  useEffect(() => {
-    setSidebarWidth(windowWidth <= 768 ? 50 : 332);
-    setContentWidth(windowWidth - sidebarWidth);
-  }, [windowWidth, sidebarWidth]);
-
+  if (isLoading) {
+    return <Loading />;
+  }
+  // useEffect(() => {
+  //   // setUser(getUserInfoQuery(user_id));
+  //   const handleResize = () => {
+  //     setWindowWidth(window.innerWidth);
+  //   };
+  //
+  //   window.addEventListener("resize", handleResize);
+  //
+  //   return () => {
+  //     window.removeEventListener("resize", handleResize);
+  //   };
+  // }, []);
+  //
+  // useEffect(() => {
+  //   setSidebarWidth(windowWidth <= 768 ? 50 : 332);
+  //   setContentWidth(windowWidth - sidebarWidth);
+  // }, [windowWidth, sidebarWidth]);
+  const user = loginUser || data;
   return (
     <div className="container">
-      <Router>
-        <Routes>
-          <Route path="/login" element={Auth(Login, true)}></Route>
-          {/*<Route path="/" element={<SideBar user={user} />}>*/}
-          {/*  <Route index element={<MainComponent />} />*/}
-          {/*  <Route path="/friend" element={<FriendList user={user} />} />*/}
-          {/*  <Route path="/chat" element={<ChatList user={user} />} />*/}
-          {/*  <Route path="/User" element={<UserProfile />} />*/}
-          {/*</Route>*/}
-
-          {/*<Route path="/friend_s" element={<SideBar user={user} />}>*/}
-          {/*  /!*<Route path=":friendid" element={<MainComponent/>}/>*!/*/}
-          {/*</Route>*/}
-
-          {/*<Route path="/ChatRoom" element={<SideBar user={user} />}>*/}
-          {/*  <Route path=":room_id" element={<ChatRoom user={user} />} />*/}
-          {/*</Route>*/}
-
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
+      <Routes>
+        // Auth에 뭐 줘야 할지 헷갈리면 걍 True로 두셈 ㅇㅇ 아니면 주석 처리하거나 // Auth 페이지에 유저를 주는 이유는 로그인이 되어 있지 않으면
+        로그인 페이지로 이동하기 위함
+        <Route path={"/"} element={Auth(MainComponent, false)}></Route>
+        <Route path="/friend" element={Auth(FriendList, false)}></Route>
+        {/*<Route path="/chat" element={Auth(ChatList, true, user)}></Route>*/}
+        {/*<Route path="/User" element={Auth(UserProfile, true, user)}></Route>*/}
+      </Routes>
+      <Routes>
+        // Auth 페이지에 true를 주는 이유는 로그인이 되어있어야만 접근 가능 하도록 하기 위함
+        <Route path="/login" element={Auth(Login, false)}></Route>
+        //그 외에는 그냥 접근 해도 되는 것들임 ㅇㅇ
+        {/*<Route path="/" element={<SideBar user={user} />}>*/}
+        {/*  <Route index element={<MainComponent />} />*/}
+        {/*  <Route path="/friend" element={<FriendList user={user} />} />*/}
+        {/*  <Route path="/chat" element={<ChatList user={user} />} />*/}
+        {/*<Route path="/User" element={<UserProfile />} />*/}
+        {/*</Route>*/}
+        {/*<Route path="/friend_s" element={<SideBar user={user} />}>*/}
+        {/*  /!*<Route path=":friendid" element={<MainComponent/>}/>*!/*/}
+        {/*</Route>*/}
+        {/*<Route path="/ChatRoom" element={<SideBar user={user} />}>*/}
+        {/*  <Route path=":room_id" element={<ChatRoom user={user} />} />*/}
+        {/*</Route>*/}
+        {/*<Route path="*" element={<Navigate to="/" />} />*/}
+      </Routes>
     </div>
   );
 }
