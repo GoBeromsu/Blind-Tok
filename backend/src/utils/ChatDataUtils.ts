9a05ab88d24c73267c0f4dae757459dd41c018fe
choidsajfisdjfk <tmdwnwlq1@naver.com>
2023-05-20 15:55:51 +0900

    socket-server => backend

---
+++ backend/src/utils/ChatDataUtils.ts
@@ -0,0 +1,59 @@
+import {type} from "os";
+
+interface ChatData {
+  room_id: string;
+  room_data: any;
+}
+// room_data 의 데이터 셋 추가 예정
+interface room_d {}
+
+var data: ChatData[];
+
+export function getData(room_id: string, data_num: number, opt: number) {
+  let index: number = data.findIndex(data => data.room_id === room_id);
+  let data_t: any;
+  if (opt == 1) {
+    data_t = data[index].room_data;
+    data[index].room_data = data_t.filter((data: any) => data.num <= data_num);
+  } else {
+    data_t = data[index].room_data.filter((data: any) => data.num > data_num);
+  }
+  console.log("getData");
+  return data_t;
+}
+
+export function createData(room_id: string) {
+  if (data.find(data => data.room_id === room_id)) return;
+  let data_n: ChatData = {room_id: room_id, room_data: []};
+  data = [...data, data_n];
+  console.log("createData : ");
+  console.log(data);
+}
+
+export function setData(room_id: string, data_t: any) {
+  let index: number = data.findIndex(data => data.room_id === room_id);
+  if (index === -1) {
+    createData(room_id);
+    index = data.length - 1;
+  }
+  let room_data_n: any;
+  if (typeof data === "undefined" || data === null) data = [];
+  else room_data_n = data[index].room_data;
+
+  data[index] = {room_id: room_id, room_data: [...room_data_n, data_t]};
+  console.log("setData : ");
+  console.log(data);
+}
+
+export function getData_D() {
+  return data;
+}
+export function setData_D(data_r: ChatData[]) {
+  data = data_r;
+}
+
+// test
+export function show_d() {
+  console.log("data : ");
+  console.log(data);
+}
--- frontend/src/socket/index.ts
+++ frontend/src/socket/index.ts
@@ -2,7 +2,7 @@ import {Socket, io} from "socket.io-client";
 import {updateChat} from "@views/Chat/ChatRoom";
 import {setList} from "@views/Chat/ChatList";
 import {setChatList} from "@data/Chat/chat_list";
-import {rec, updateData_s} from "@data/Chat/chat_data";
+import {updateChatData, updateData_s} from "@data/Chat/chat_data";
 
 var socket: Socket = io("");
 
@@ -11,7 +11,7 @@ export function createSocket(add: string = "", user: any) {
     return;
   }
 
-  socket = add === "" ? io("http://localhost:4000") : io(add);
+  socket = add === "" ? io("http://localhost:4001") : io(add);
 
   socket.on("disconnect", reason => {
     socket = io("");
@@ -20,7 +20,7 @@ export function createSocket(add: string = "", user: any) {
 
   socket.on("receive_message", (data: any) => {
     console.log(data);
-    updateChat(rec(data));
+    updateChat(updateChat(data));
   });
 
   socket.on("update_roomList", (data: any) => {
