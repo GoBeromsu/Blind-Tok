d228301ef2432b5684a46378cfac85dc1883c599
choidsajfisdjfk <tmdwnwlq1@naver.com>
2023-06-03 18:00:20 +0900

    방 삭제 에러 수정

--- backend/src/chat/ChatUserUtils.ts
+++ backend/src/chat/ChatUserUtils.ts
@@ -32,8 +32,7 @@ export function removeRoomList(roomid: number, userid: string) {
     return;
   }
 
-  chatUserData[userid].roomlist = chatUserData[userid].roomlist.filter(room => room.roomid !== roomid);
-
+  chatUserData[userid].roomlist = chatUserData[userid].roomlist.filter(room => room !== roomid);
   console.log("removeRoomFromUser: ", chatUserData);
 }
 
