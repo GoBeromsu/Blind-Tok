f8ea75bb0430b71f2a2c03e9b60ee5104f96ed59
goberomsu <gobeumsu@gmail.com>
2023-06-04 16:53:14 +0900

    Feat : UserRegistry 추가
    이제는 변수에 userSession을 저장하고 직접 접근 하는 것이 아니라
    
    UserSession에 대한 책임을 질 클래스를 생성하였다

---
+++ backend/src/chat/UserRegistry.ts
@@ -0,0 +1,40 @@
+import UserSession from "./UserSession";
+
+export default class {
+  private usersById: {[key: string]: UserSession} = {};
+  private userByName: {[key: string]: UserSession} = {};
+
+  register(user: UserSession) {
+    this.usersById[user.userid] = user;
+    this.userByName[user.name] = user;
+  }
+
+  unregister(userId: string) {
+    const user = this.usersById[userId];
+    if (user) {
+      delete this.usersById[userId];
+      delete this.userByName[user.name];
+    }
+  }
+
+  getByName(name: string) {
+    return this.userByName[name];
+  }
+
+  getById(userId: string) {
+    return this.usersById[userId];
+  }
+
+  removeById(userId: string) {
+    const userSession = this.usersById[userId];
+    if (!userSession) return;
+    delete this.usersById[userId];
+    delete this.userByName[userSession.name];
+  }
+
+  getUsersByRoom(roomId: string): UserSession[] {
+    const allUsers = Object.values(this.usersById);
+    const usersInRoom = allUsers.filter(user => user.roomlist.includes(roomId));
+    return usersInRoom;
+  }
+}
