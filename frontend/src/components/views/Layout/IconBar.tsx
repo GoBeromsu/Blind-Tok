f38515aa14b6221b9906aca5b912169c3cebb2b7
Choo-bi <39858246+Choo-bi@users.noreply.github.com>
2023-06-06 03:16:24 +0900

    로그아웃 기능 구현중
    yarn add 해주세요

--- frontend/src/components/views/Layout/IconBar.tsx
+++ frontend/src/components/views/Layout/IconBar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 import {Link} from "react-router-dom";
 import {IconButton} from "@mui/material";
 import PeopleIcon from "@mui/icons-material/People";
@@ -9,8 +10,15 @@ import NotificationsIcon from "@mui/icons-material/Notifications";
 import SettingsIcon from "@mui/icons-material/Settings";
 import LogoutIcon from "@mui/icons-material/Logout";
 import Br from "./Br";
-
+import {Cookies} from "react-cookie";
 const IconBar = () => {
+  let navigate = useNavigate();
+  const cookies = new Cookies();
+  const handleLogout = () => {
+    cookies.remove("access_token", {path: "/", domain: "localhost"});
+    navigate("/login");
+  };
+
   return (
     <>
       <div className="icon_sidebar">
@@ -60,7 +68,7 @@ const IconBar = () => {
           </Link>
           <Br />
         </div>
-        <div className="item">
+        <div className="item" onClick={handleLogout}>
           <IconButton>
             <LogoutIcon style={{fontSize: 28}} />
           </IconButton>
