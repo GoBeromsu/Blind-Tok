57723210b7e89135b41e171c122c36e3ead6cb09
goberomsu <gobeumsu@gmail.com>
2023-06-06 00:28:20 +0900

    Fix : Socket 의존성 분리
    - 채팅 등 리액트 컴포넌트 안에서 렌더링 되어야 하는 것은 리액트로 가져감
    - 그 외에 sendMessage 등은 상관 없으니까 그대로 둔다

--- frontend/src/data/chat/index.ts
+++ frontend/src/data/chat/index.ts
@@ -1,18 +1,17 @@
 import {Socket, io} from "socket.io-client";
 
 import {SOCKET_URL} from "../../consonants";
-import {recMessage} from "@data/chat/ChattingController";
+import {updateChat} from "@views/Chat/ChatRoom";
+import {updateChatData, updateData_s} from "@data/chat/chat_data";
+import {setList} from "@views/Chat/ChatList";
+import {addChatList, setChatList, setListMessage} from "@data/chat/chat_list";
 
-const socket: Socket = io(SOCKET_URL);
+export const socket: Socket = io(SOCKET_URL);
 socket.on("disconnect", reason => {
   console.log("socket : disconnect");
 });
 
-socket.on("message", (message: any) => {
-  recMessage(message);
-});
-
 export function sendMessage(data: any, opt: string) {
-  console.log("sendMessage : ", data);
+  console.log("sendMessage : ", data, ", 옵션 : ", opt);
   socket.emit(opt, data);
 }
